1adb0d47536e2eaa5f805552ca0e004b8548603e
jacobadam <jacobanevitt@gmail.com>
2024-10-31 13:54:57 -0700

    add linkedin logo

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -34,6 +34,42 @@ const NavBar = () => {
               </ul>
             </nav>
           </div>
+
+          <div className="ml-auto flex items-center gap-4">
+            <a
+              href="https://www.linkedin.com/in/katie-shevlin90/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/linkedin-logo.webp" // Make sure the path is correct
+                alt="LinkedIn"
+                className="h-6 w-6"
+              />
+            </a>
+
+            {/* Mobile Menu Button */}
+            <button
+              onClick={toggleMenu}
+              className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
+            >
+              <span className="sr-only">Toggle menu</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
 
         {/* Mobile Dropdown Menu */}
