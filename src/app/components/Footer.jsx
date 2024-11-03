60b10375266450618c4bd27e1855dfebcd443450
jacobadam <jacobanevitt@gmail.com>
2024-11-03 11:30:50 -0800

    fix styling issues on value props and update alignment on footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <footer style={{ backgroundColor: "#10172A" }} className="py-6 mt-auto">
-    <div className="max-w-screen-xl mx-auto flex justify-center">
+    <div className="mx-auto flex justify-center md:justify-start">
       <span className="text-sm text-white">
         © 2024 Katie Shevlin. All Rights Reserved.
       </span>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -19,7 +19,7 @@ export default function Home() {
       </div>
 
       {/* Content Section */}
-      <div className="bg-white flex flex-col items-center justify-center mt-28 mb-4">
+      <div className="bg-white flex flex-col items-center justify-center mt-28">
         <div className="items-center px-8 mx-auto max-w-7xl lg:px-16 md:px-12 text-center">
           <p className="mt-3 text-[#10172A] text-4xl font-semibold tracking-tighter whitespace-nowrap">
             Katie Shevlin
@@ -29,7 +29,7 @@ export default function Home() {
             editor with a decade of experience{" "}
             <span className="block">producing high-quality content.</span>
           </p>
-          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-[#10172A] border border-[#10172A] rounded-lg group bg-gradient-to-br from-[#10172A] to-[#10172A] group-hover:from-[#10172A] group-hover:to-[#10172A] hover:text-white focus:outline-none">
+          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-bold text-[#10172A] border border-[#10172A] rounded-lg group bg-gradient-to-br from-[#10172A] to-[#10172A] group-hover:from-[#10172A] group-hover:to-[#10172A] hover:text-white focus:outline-none">
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
               Get in touch
             </span>
@@ -40,48 +40,48 @@ export default function Home() {
       <div className="flex-grow"></div>
 
       {/* Value Prop */}
-      <section className="text-gray-400 bg-gray-900 body-font">
-        <div className="container px-5 py-8 mx-auto">
-          <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
-            <div className="p-4 md:w-1/4 flex">
-              <div className="flex-grow pl-6">
-                <h2 className="text-white title-font font-medium mb-2 text-center">
+      <section className="text-[#10172A] bg-[#B0B0B0] body-font">
+        <div className="container px-5 2xl:py-8 mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+            <div className="p-4 flex sm:justify-center">
+              <div className="flex flex-col items-center text-center px-4 h-full">
+                <h2 className="text-[#10172A] title-font font-bold mb-2">
                   Engagement
                 </h2>
-                <p className="leading-relaxed text-base text-center">
+                <p className="leading-relaxed text-base">
                   Drawing out relatable, compelling stories that captivate and
                   connect with your audience.
                 </p>
               </div>
             </div>
-            <div className="p-4 md:w-1/4 flex">
-              <div className="flex-grow pl-6">
-                <h2 className="text-white title-font font-medium mb-2 text-center">
+            <div className="p-4 flex sm:justify-center">
+              <div className="flex flex-col  items-center text-center px-4 h-full">
+                <h2 className="text-[#10172A] title-font font-bold mb-2">
                   Strategy
                 </h2>
-                <p className="leading-relaxed text-base text-center">
+                <p className="leading-relaxed text-base">
                   Aligning content with your marketing objectives for maximum
                   impact.
                 </p>
               </div>
             </div>
-            <div className="p-4 md:w-1/4 flex">
-              <div className="flex-grow pl-6">
-                <h2 className="text-white title-font font-medium mb-2 text-center">
+            <div className="p-4 flex sm:justify-center ">
+              <div className="flex flex-col items-center text-center px-4 h-full">
+                <h2 className="text-[#10172A] title-font font-bold mb-2">
                   Reach
                 </h2>
-                <p className="leading-relaxed text-base text-center">
+                <p className="leading-relaxed text-base">
                   Using SEO techniques to drive organic traffic and get your
                   content in front of the right people.
                 </p>
               </div>
             </div>
-            <div className="p-4 md:w-1/4 flex">
-              <div className="flex-grow pl-6">
-                <h2 className="text-white title-font font-medium mb-2 text-center">
+            <div className="p-4 flex sm:justify-center ">
+              <div className="flex flex-col items-center text-center px-4 h-full">
+                <h2 className="text-[#10172A] title-font font-bold mb-2">
                   Performance
                 </h2>
-                <p className="leading-relaxed text-base text-center">
+                <p className="leading-relaxed text-base">
                   Tracking analytics to continually optimise content and drive
                   success.
                 </p>
@@ -89,10 +89,10 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="flex justify-center mt-8">
+          <div className="flex justify-center">
             <a
               href="/services"
-              className="text-white text-center hover:underline"
+              className="text-[#10172A] text-center font-semibold hover:underline"
             >
               Learn more about my services here.
             </a>
