1280ff8a8122abc3eb982c7f97135535200e611d
jacobadam <jacobanevitt@gmail.com>
2024-11-04 11:17:02 -0800

    decrease font size for smaller devices

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer style={{ backgroundColor: "#10172A" }} className="py-6 mt-auto">
     <div className="mx-auto flex justify-center md:justify-start">
-      <span className="text-sm text-white">
+      <span className="text-xs sm:text-sm text-white">
         © 2024 Katie Shevlin. All Rights Reserved.
       </span>
     </div>
