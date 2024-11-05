939ec252b6eeaf0b42ce293f35d6e8b97e81c334
jacobadam <jacobanevitt@gmail.com>
2024-11-05 11:13:53 -0800

    update portfolio template

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,3 +1,259 @@
 export default function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <section className="flex-grow">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+          <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-portfolio">
+            Porfolio
+          </h2>
+          <p className="font-light text-portfolio sm:text-xl">
+            We use an agile approach to test assumptions and connect with the
+            needs of your audience early and often.
+          </p>
+        </div>
+        <div className="grid gap-8 lg:grid-cols-4">
+          <article className="p-6 bg-portfolio rounded-lg shadow-md">
+            <h2 className="mb-2 text-xl font-semibold tracking-tight text-stone-100">
+              <a href="#">How to quickly deploy a static website</a>
+            </h2>
+            <p className="mb-5 font-light text-stone-100">
+              Static websites are now used to bootstrap lots of websites and are
+              becoming the basis for a variety of tools that even influence both
+              web designers and developers.
+            </p>
+            <div className="flex justify-between items-center">
+              <a
+                href="#"
+                className="inline-flex items-center font-medium text-stone-100 dark:text-primary-500 hover:underline"
+              >
+                Read more
+                <svg
+                  className="ml-2 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </article>
+          <article className="p-6 bg-portfolio rounded-lg shadow-md">
+            <h2 className="mb-2 text-xl font-semibold tracking-tight text-stone-100">
+              <a href="#">How to quickly deploy a static website</a>
+            </h2>
+            <p className="mb-5 font-light text-stone-100">
+              Static websites are now used to bootstrap lots of websites and are
+              becoming the basis for a variety of tools that even influence both
+              web designers and developers.
+            </p>
+            <div className="flex justify-between items-center">
+              <a
+                href="#"
+                className="inline-flex items-center font-medium text-stone-100 dark:text-primary-500 hover:underline"
+              >
+                Read more
+                <svg
+                  className="ml-2 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </article>
+          <article className="p-6 bg-portfolio rounded-lg shadow-md">
+            <h2 className="mb-2 text-xl font-semibold tracking-tight text-stone-100">
+              <a href="#">How to quickly deploy a static website</a>
+            </h2>
+            <p className="mb-5 font-light text-stone-100">
+              Static websites are now used to bootstrap lots of websites and are
+              becoming the basis for a variety of tools that even influence both
+              web designers and developers.
+            </p>
+            <div className="flex justify-between items-center">
+              <a
+                href="#"
+                className="inline-flex items-center font-medium text-stone-100 dark:text-primary-500 hover:underline"
+              >
+                Read more
+                <svg
+                  className="ml-2 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </article>
+          <article className="p-6 bg-portfolio rounded-lg shadow-md">
+            <h2 className="mb-2 text-xl font-semibold tracking-tight text-stone-100">
+              <a href="#">How to quickly deploy a static website</a>
+            </h2>
+            <p className="mb-5 font-light text-stone-100">
+              Static websites are now used to bootstrap lots of websites and are
+              becoming the basis for a variety of tools that even influence both
+              web designers and developers.
+            </p>
+            <div className="flex justify-between items-center">
+              <a
+                href="#"
+                className="inline-flex items-center font-medium text-stone-100 dark:text-primary-500 hover:underline"
+              >
+                Read more
+                <svg
+                  className="ml-2 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </article>
+          <article className="p-6 bg-portfolio rounded-lg shadow-md">
+            <h2 className="mb-2 text-xl font-semibold tracking-tight text-stone-100">
+              <a href="#">How to quickly deploy a static website</a>
+            </h2>
+            <p className="mb-5 font-light text-stone-100">
+              Static websites are now used to bootstrap lots of websites and are
+              becoming the basis for a variety of tools that even influence both
+              web designers and developers.
+            </p>
+            <div className="flex justify-between items-center">
+              <a
+                href="#"
+                className="inline-flex items-center font-medium text-stone-100 dark:text-primary-500 hover:underline"
+              >
+                Read more
+                <svg
+                  className="ml-2 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </article>
+          <article className="p-6 bg-portfolio rounded-lg shadow-md">
+            <h2 className="mb-2 text-xl font-semibold tracking-tight text-stone-100">
+              <a href="#">How to quickly deploy a static website</a>
+            </h2>
+            <p className="mb-5 font-light text-stone-100">
+              Static websites are now used to bootstrap lots of websites and are
+              becoming the basis for a variety of tools that even influence both
+              web designers and developers.
+            </p>
+            <div className="flex justify-between items-center">
+              <a
+                href="#"
+                className="inline-flex items-center font-medium text-stone-100 dark:text-primary-500 hover:underline"
+              >
+                Read more
+                <svg
+                  className="ml-2 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </article>
+          <article className="p-6 bg-portfolio rounded-lg shadow-md">
+            <h2 className="mb-2 text-xl font-semibold tracking-tight text-stone-100">
+              <a href="#">How to quickly deploy a static website</a>
+            </h2>
+            <p className="mb-5 font-light text-stone-100">
+              Static websites are now used to bootstrap lots of websites and are
+              becoming the basis for a variety of tools that even influence both
+              web designers and developers.
+            </p>
+            <div className="flex justify-between items-center">
+              <a
+                href="#"
+                className="inline-flex items-center font-medium text-stone-100 dark:text-primary-500 hover:underline"
+              >
+                Read more
+                <svg
+                  className="ml-2 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </article>
+          <article className="p-6 bg-portfolio rounded-lg shadow-md">
+            <h2 className="mb-2 text-xl font-semibold tracking-tight text-stone-100">
+              <a href="#">How to quickly deploy a static website</a>
+            </h2>
+            <p className="mb-5 font-light text-stone-100">
+              Static websites are now used to bootstrap lots of websites and are
+              becoming the basis for a variety of tools that even influence both
+              web designers and developers.
+            </p>
+            <div className="flex justify-between items-center">
+              <a
+                href="#"
+                className="inline-flex items-center font-medium text-stone-100 dark:text-primary-500 hover:underline"
+              >
+                Read more
+                <svg
+                  className="ml-2 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
 }
