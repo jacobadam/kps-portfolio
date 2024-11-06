ff23cb5342ea9bb9a606e04453bc78691481b264
jacobadam <jacobanevitt@gmail.com>
2024-11-06 13:42:45 -0800

    fix styling on portfolio cards

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -16,7 +16,7 @@ export default function Portfolio() {
       <div className="py-4 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-6">
         <div className="grid gap-8 grid-cols-2 lg:grid-cols-4">
           {/* Article 1 */}
-          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm">
+          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm flex flex-col">
             <header className="p-4 bg-gray-200 text-center">
               <h2 className="text-xl font-bold text-portfolio">Technology</h2>
             </header>
@@ -25,7 +25,7 @@ export default function Portfolio() {
               src="/perfect-pizza.webp"
               className="h-56 w-full object-cover"
             />
-            <div className="p-4 sm:p-6">
+            <div className="p-4 sm:p-6 flex flex-col grow">
               <a
                 href="https://www.toptal.com/product-managers/ecommerce/customer-research-tool"
                 target="_blank"
@@ -36,7 +36,7 @@ export default function Portfolio() {
                   Measure Taste
                 </h3>
               </a>
-              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 mb-4">
                 Customer research in the food sector is expensive and
                 time-consuming. Here’s how one global pizza company used its own
                 food-ordering app to get new levels of insight into its
@@ -46,7 +46,7 @@ export default function Portfolio() {
                 href="https://www.toptal.com/product-managers/ecommerce/customer-research-tool"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-portfolio"
+                className="group mt-auto inline-flex items-center gap-1 text-sm font-medium text-portfolio"
               >
                 Read more
                 <span
@@ -60,7 +60,7 @@ export default function Portfolio() {
           </article>
 
           {/* Article 2 */}
-          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm">
+          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm flex flex-col">
             <header className="p-4 bg-gray-200 text-center">
               <h2 className="text-xl font-bold text-portfolio">Lifestyle</h2>
             </header>
@@ -70,7 +70,7 @@ export default function Portfolio() {
               className="h-56 w-full object-cover"
             />
 
-            <div className="p-4 sm:p-6">
+            <div className="p-4 sm:p-6 flex flex-col grow">
               <a
                 href="https://www.pursuitmedia.co/2020/11/30/7-must-visit-coffee-shops-in-vancouver-canada/"
                 target="_blank"
@@ -81,8 +81,7 @@ export default function Portfolio() {
                 </h3>
               </a>
 
-              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-                {" "}
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 mb-4">
                 Whether it’s an essential part of your morning routine, a
                 hump-day treat, or you simply need a break from your desk, we
                 all love a coffee shop. Vancouver has an abundance of choice in
@@ -94,7 +93,7 @@ export default function Portfolio() {
                 href="https://www.pursuitmedia.co/2020/11/30/7-must-visit-coffee-shops-in-vancouver-canada/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-portfolio"
+                className="group mt-auto inline-flex items-center gap-1 text-sm font-medium text-portfolio"
               >
                 Read more
                 <span
@@ -108,7 +107,7 @@ export default function Portfolio() {
           </article>
 
           {/* Article 3 */}
-          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm">
+          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm flex flex-col">
             <header className="p-4 bg-gray-200 text-center">
               <h2 className="text-xl font-bold text-portfolio">Finance</h2>
             </header>
@@ -118,7 +117,7 @@ export default function Portfolio() {
               className="h-56 w-full object-cover"
             />
 
-            <div className="p-4 sm:p-6">
+            <div className="p-4 sm:p-6 flex flex-col grow">
               <a
                 href="/metrobank-interview.pdf"
                 target="_blank"
@@ -129,7 +128,7 @@ export default function Portfolio() {
                 </h3>
               </a>
 
-              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 mb-4">
                 Metro Bank is set to become the next big name in franchise
                 business lending. Katie Shevlin meets founder and chairman
                 Vernon Hill II
@@ -139,7 +138,7 @@ export default function Portfolio() {
                 href="/metrobank-interview.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-portfolio"
+                className="group mt-auto inline-flex items-center gap-1 text-sm font-medium text-portfolio"
               >
                 Read more
                 <span
@@ -153,29 +152,33 @@ export default function Portfolio() {
           </article>
 
           {/* Article 4 */}
-          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm">
+          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm flex flex-col">
             <header className="p-4 bg-gray-200 text-center">
               <h2 className="text-xl font-bold text-portfolio">Healthcare</h2>
             </header>
             <img alt="" src="/ai.webp" className="h-56 w-full object-cover" />
 
-            <div className="p-4 sm:p-6">
+            <div className="p-4 sm:p-6 flex flex-col grow">
               <a href="/hp.pdf" target="_blank" rel="noopener noreferrer">
                 <h3 className="text-lg font-medium text-portfolio">
                   AI and the NHS Long Term Plan
                 </h3>
               </a>
 
-              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-                The role that AI could play in the UK health sector in the next
-                decade.
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 mb-4">
+                Artificial intelligence (AI) is firmly embedded in modern life.
+                It is in our homes in the form of personal digital assistants,
+                helping detect fraud in our banking systems, identifying
+                inappropriate content on our social media feeds. As prevalent as
+                it may already feel, however, AI is still very much in its
+                infancy.
               </p>
 
               <a
                 href="/hp.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-portfolio"
+                className="group mt-auto inline-flex items-center gap-1 text-sm font-medium text-portfolio"
               >
                 Read more
                 <span
@@ -189,7 +192,7 @@ export default function Portfolio() {
           </article>
 
           {/* Article 5 */}
-          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm">
+          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm flex flex-col">
             <header className="p-4 bg-gray-200 text-center">
               <h2 className="text-xl font-bold text-portfolio">
                 Current Affairs
@@ -201,7 +204,7 @@ export default function Portfolio() {
               className="h-56 w-full object-cover"
             />
 
-            <div className="p-4 sm:p-6">
+            <div className="p-4 sm:p-6 flex flex-col grow">
               <a
                 href="https://www.nzherald.co.nz/bay-of-plenty-times/news/editorial-brexit-a-leap-into-unknown-and-here-be-monsters/KCEMVA7XN4F7WCJVDQBGHDUSCM/#google_vignette"
                 target="_blank"
@@ -212,7 +215,7 @@ export default function Portfolio() {
                 </h3>
               </a>
 
-              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 mb-4">
                 "Should I stay or should I go?" asked The Clash in 1982. And, on
                 June 23, UK citizens will be asking themselves just that as they
                 walk into voting booths across Britain and make the
@@ -223,7 +226,7 @@ export default function Portfolio() {
                 href="https://www.nzherald.co.nz/bay-of-plenty-times/news/editorial-brexit-a-leap-into-unknown-and-here-be-monsters/KCEMVA7XN4F7WCJVDQBGHDUSCM/#google_vignette"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-portfolio"
+                className="group mt-auto inline-flex items-center gap-1 text-sm font-medium text-portfolio"
               >
                 Read more
                 <span
@@ -237,7 +240,7 @@ export default function Portfolio() {
           </article>
 
           {/* Article 6 */}
-          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm">
+          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm flex flex-col">
             <header className="p-4 bg-gray-200 text-center">
               <h2 className="text-xl font-bold text-portfolio">Business</h2>
             </header>
@@ -247,7 +250,7 @@ export default function Portfolio() {
               className="h-56 w-full object-cover"
             />
 
-            <div className="p-4 sm:p-6">
+            <div className="p-4 sm:p-6 flex flex-col grow">
               <a
                 href="https://www.toptal.com/project-managers/project-management-consultant/organizational-change-management-coaching"
                 target="_blank"
@@ -258,7 +261,7 @@ export default function Portfolio() {
                 </h3>
               </a>
 
-              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 mb-4">
                 Whether it’s introducing a new organizational structure, tool,
                 or process, project managers are often agents of change. Help
                 team members adapt and thrive with these four strategies.
@@ -268,7 +271,7 @@ export default function Portfolio() {
                 href="https://www.toptal.com/project-managers/project-management-consultant/organizational-change-management-coaching"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-portfolio"
+                className="group mt-auto inline-flex items-center gap-1 text-sm font-medium text-portfolio"
               >
                 Read more
                 <span
@@ -282,7 +285,7 @@ export default function Portfolio() {
           </article>
 
           {/* Article 7 */}
-          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm">
+          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm flex flex-col">
             <header className="p-4 bg-gray-200 text-center">
               <h2 className="text-xl font-bold text-portfolio">
                 Diversity and Inclusion
@@ -293,7 +296,7 @@ export default function Portfolio() {
               src="/gender-imbalance.webp"
               className="h-56 w-full object-cover"
             />
-            <div className="p-4 sm:p-6">
+            <div className="p-4 sm:p-6 flex flex-col grow">
               <a
                 href="https://www.digitalhealth.net/2020/05/addressing-gender-imbalance-will-take-time-but-change-needs-to-start-now/"
                 target="_blank"
@@ -305,7 +308,7 @@ export default function Portfolio() {
                 </h3>
               </a>
 
-              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 mb-4">
                 Gender imbalance in digital health has long been debated and
                 Katie Shevlin from Inhealth Communications looks into why
                 addressing the issue will take time but has to be driven now.
@@ -315,7 +318,7 @@ export default function Portfolio() {
                 href="https://www.digitalhealth.net/2020/05/addressing-gender-imbalance-will-take-time-but-change-needs-to-start-now/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-portfolio"
+                className="group mt-auto inline-flex items-center gap-1 text-sm font-medium text-portfolio"
               >
                 Read more
                 <span
@@ -329,7 +332,7 @@ export default function Portfolio() {
           </article>
 
           {/* Article 8 */}
-          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm">
+          <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm flex flex-col">
             <header className="p-4 bg-gray-200 text-center">
               <h2 className="text-xl font-bold text-portfolio">Trends</h2>
             </header>
@@ -338,7 +341,7 @@ export default function Portfolio() {
               src="/ar-marketing.webp"
               className="h-56 w-full object-cover"
             />
-            <div className="p-4 sm:p-6">
+            <div className="p-4 sm:p-6 flex flex-col grow">
               <a
                 href="https://www.toptal.com/marketing/digital/augmented-reality-marketing"
                 target="_blank"
@@ -349,7 +352,7 @@ export default function Portfolio() {
                 </h3>
               </a>
 
-              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 mb-4">
                 Augmented reality is redefining how brands engage customers, and
                 top brands are offering exciting immersive experiences. Explore
                 current applications and what the future of AR in marketing
@@ -360,7 +363,7 @@ export default function Portfolio() {
                 href="https://www.toptal.com/marketing/digital/augmented-reality-marketing"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-portfolio"
+                className="group mt-auto inline-flex items-center gap-1 text-sm font-medium text-portfolio"
               >
                 Read more
                 <span
