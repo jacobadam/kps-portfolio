36b1e5069129bd6dcb25d92459e179e4d06cc5b7
jacobadam <jacobanevitt@gmail.com>
2024-11-06 18:38:04 -0800

    update metadata description, add logo, add sitemap

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,8 +3,9 @@ import NavBar from "../app/components/NavBar";
 import Footer from "../app/components/Footer";
 
 export const metadata = {
-  title: "Katie Shelin Profile",
-  description: "",
+  title: "Katie Shelin Portfolio",
+  description:
+    "Professional writing, editing, SEO, and strategy services to elevate your brand. Get expert support to ensure your content has impact and meets your marketing goals.",
 };
 
 export default function RootLayout({ children }) {
