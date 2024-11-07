5b502679f13abeb8f591d2867016e0fbdd201cc0
jacobadam <jacobanevitt@gmail.com>
2024-11-06 20:06:14 -0800

    remove 404

--- src/app/(404)/page.js
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="grid h-screen place-content-center px-4 bg-stone-100">
-      <h1 className="uppercase tracking-widest text-portfolio">
-        404 | Not Found
-      </h1>
-    </div>
-  );
-}
