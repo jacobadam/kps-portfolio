92b5b24dc1824c81ff1d0d5c80800808ebf480fb
jacobadam <jacobanevitt@gmail.com>
2024-11-07 15:53:54 -0800

    add padding to copyright

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer className="py-6 mt-auto bg-portfolio">
     <div className="mx-auto flex justify-center md:justify-start">
-      <span className="text-xs sm:text-sm text-stone-100">
+      <span className="text-xs sm:text-sm text-stone-100 pl-4">
         © 2024 Katie Shevlin. All Rights Reserved.
       </span>
     </div>
