71fb93a208630e09e53cf4a702bc4db00aa69158
jacobadam <jacobanevitt@gmail.com>
2024-11-06 17:37:02 -0800

    add 404 and update metadata

---
+++ src/app/(404)/page.js
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="grid h-screen place-content-center px-4 bg-stone-100">
+      <h1 className="uppercase tracking-widest text-portfolio">
+        404 | Not Found
+      </h1>
+    </div>
+  );
+}
