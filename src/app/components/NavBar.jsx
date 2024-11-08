ed77c349d8d55c54d8163e7fb130a5d87ee4390f
jacobadam <jacobanevitt@gmail.com>
2024-11-08 08:55:09 -0800

    update logo

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
     <div>
       <header className="bg-portfolio">
         <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-          <a href="/" target="_blank" rel="noopener noreferrer">
+          <a href="/">
             <img src="/ks-logo.svg" alt="logo" className="h-12 w-12" />
           </a>
 
