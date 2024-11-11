2657ea8e123ddeb0b03192be9938df688a5b19e3
jacobadam <jacobanevitt@gmail.com>
2024-11-10 19:31:26 -0800

    update tailwind congfig and optimize image

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,12 +10,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      ...defaultTheme.screens,
+      xxs: "378px",
+      xs: "540px",
+      "short-lg": { raw: "(min-width: 1024px) and (max-height: 766px)" },
+    },
     extend: {
-      screens: {
-        xxs: "378px",
-        xs: "540px",
-        "short-lg": { raw: "(min-width: 1024px) and (max-height: 766px)" },
-      },
       colors: {
         portfolio: "#172754",
       },
