3f5342820d54d7789e7f8ae2a3c51edec5fc1c3e
jacobadam <jacobanevitt@gmail.com>
2024-11-10 19:58:16 -0800

    update footer with accessability stuff and learn how to spell accessibility

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="py-6 mt-auto bg-portfolio">
+  <footer className="py-6 mt-auto bg-portfolio" role="contentinfo">
     <div className="mx-auto flex justify-center md:justify-start">
-      <span className="text-xs sm:text-sm text-stone-100 pl-4">
+      <span
+        className="text-xs sm:text-sm text-stone-100 pl-4"
+        aria-live="polite"
+      >
         © 2024 Katie Shevlin. All Rights Reserved.
       </span>
     </div>
