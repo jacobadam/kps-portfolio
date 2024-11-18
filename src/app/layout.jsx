b493d61487349910681fd943d84180b2bbbf5929
jacobadam <jacobanevitt@gmail.com>
2024-11-18 15:00:40 -0800

    update metadara for SEO

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,15 +6,30 @@ export const metadata = {
   title: "Content Services by Katie Shevlin",
   description:
     "Professional writing, editing, SEO, and strategy services to elevate your brand. Get expert support to ensure your content has impact and meets your marketing goals.",
+  keywords: [
+    "freelance editor",
+    "freelance writer",
+    "SEO",
+    "strategy",
+    "content strategy",
+    "editor",
+    "writer",
+  ],
+  robots: "index, follow",
+  author: "Katie Shevlin",
+  language: "en",
+  viewport: "width=device-width, initial-scale=1.0",
+  canonical: "https://www.katieshevlin.com",
+
   openGraph: {
-    url: "https://katieshevlin.com",
+    url: "https://www.katieshevlin.com",
     type: "website",
     title: "Content Services by Katie Shevlin",
     description:
       "Professional writing, editing, SEO, and strategy services to elevate your brand. Get expert support to ensure your content has impact and meets your marketing goals.",
     images: [
       {
-        url: "https://katieshevlin.com/hero-image.webp",
+        url: "https://www.katieshevlin.com/hero-image.webp",
         width: 1200,
         height: 630,
         alt: "Hero Image",
@@ -23,19 +38,29 @@ export const metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    domain: "katieshevlin.com",
-    url: "https://katieshevlin.com",
+    domain: "www.katieshevlin.com",
+    url: "https://www.katieshevlin.com",
     title: "Content Services by Katie Shevlin",
     description:
       "Professional writing, editing, SEO, and strategy services to elevate your brand. Get expert support to ensure your content has impact and meets your marketing goals.",
     images: [
       {
-        url: "https://katieshevlin.com/hero-image.webp",
+        url: "https://www.katieshevlin.com/hero-image.webp",
         width: 1200,
         height: 630,
         alt: "Hero Image",
       },
     ],
+  },
+  structuredData: {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    name: "Content Services by Katie Shevlin",
+    url: "https://www.katieshevlin.com",
+    logo: "https://www.katieshevlin.com/logo.webp",
+    sameAs: ["https://www.linkedin.com/in/katie-shevlin"],
+    description:
+      "Professional writing, editing, SEO, and strategy services to elevate your brand. Get expert support to ensure your content has impact and meets your marketing goals.",
   },
 };
 
