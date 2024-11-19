2c1c344b912d2fe3cd49c16bafb69b29e5d0152d
jacobadam <jacobanevitt@gmail.com>
2024-11-19 12:34:49 -0800

    use Script to defer GTM after page load

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
@@ -69,20 +70,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script
-          async
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-4SB9H3JJVN"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-4SB9H3JJVN');
-            `,
-          }}
+          strategy="afterInteractive"
         />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4SB9H3JJVN');
+          `}
+        </Script>
       </head>
       <body className="flex flex-col min-h-screen">
         <NavBar />
