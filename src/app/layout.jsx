79403d30cc8280e76f12330c7d3175b1d1bee3aa
jacobadam <jacobanevitt@gmail.com>
2024-11-19 12:25:05 -0800

    add google tag stuff

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -68,6 +68,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4SB9H3JJVN"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4SB9H3JJVN');
+            `,
+          }}
+        />
+      </head>
       <body className="flex flex-col min-h-screen">
         <NavBar />
         <main className="flex-grow">{children}</main>
