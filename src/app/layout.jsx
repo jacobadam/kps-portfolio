d361df44e30e3f817a4ba99c92a8d9ccea8db19c
jacobadam <jacobanevitt@gmail.com>
2024-11-19 12:43:27 -0800

    revert to afterInteractive

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -72,9 +72,9 @@ export default function RootLayout({ children }) {
       <head>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-4SB9H3JJVN"
-          strategy="lazyOnload"
+          strategy="afterInteractive"
         />
-        <Script id="google-analytics" strategy="lazyOnload">
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
