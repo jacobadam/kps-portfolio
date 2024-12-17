63ad2fb30d3f2e4a81c25c5a5b8c4b0b68568563
jacobadam <jacobanevitt@gmail.com>
2024-12-17 08:53:09 -0800

    remove GTM

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,3 @@
-import Script from "next/script";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
@@ -69,20 +68,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-4SB9H3JJVN"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-4SB9H3JJVN');
-          `}
-        </Script>
-      </head>
       <body className="flex flex-col min-h-screen">
         <NavBar />
         <main className="flex-grow">{children}</main>
