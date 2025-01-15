b8b50d2a10e8ec9bc00b9de3c219b644fdc2ab24
jacobadam <jacobanevitt@gmail.com>
2025-01-14 17:59:26 -0800

    update copyright

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => (
         className="text-xs sm:text-sm text-stone-100 pl-4"
         aria-live="polite"
       >
-        © 2024 Katie Shevlin. All Rights Reserved.
+        © 2025 Katie Shevlin. All Rights Reserved.
       </span>
     </div>
   </footer>
