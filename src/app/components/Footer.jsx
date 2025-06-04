11563065d2b8b45b5e12fe999c004a6553624fa4
jacobadam <jacobanevitt@gmail.com>
2025-06-04 15:40:21 -0600

    update footer text

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
-const Footer = () => (
-  <footer className="py-6 mt-auto bg-portfolio" role="contentinfo">
-    <div className="mx-auto flex justify-center md:justify-start">
-      <span
-        className="text-xs sm:text-sm text-stone-100 pl-4"
-        aria-live="polite"
-      >
-        © 2025 Katie Shevlin. All Rights Reserved.
-      </span>
-    </div>
-  </footer>
-);
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="py-6 mt-auto bg-portfolio" role="contentinfo">
+      <div className="mx-auto flex justify-center md:justify-start">
+        <span
+          className="text-xs sm:text-sm text-stone-100 pl-4"
+          aria-live="polite"
+        >
+          © {currentYear} Greenhouse Web Designs. All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
